9f26876ead208d8bc520036590caaf1f793777bb
kauanny-cmd <kauannyvieira8@gmail.com>
2024-08-22 19:40:12 -0300

    feat: initial layout

---
+++ main.js
@@ -0,0 +1,21 @@
+const carousel = document.querySelector('#carouselExampleIndicators');
+let startX = 0;
+let endX = 0;
+
+carousel.addEventListener('touchstart', (e) => {
+  startX = e.touches[0].clientX;
+});
+
+carousel.addEventListener('touchmove', (e) => {
+  endX = e.touches[0].clientX;
+});
+
+carousel.addEventListener('touchend', () => {
+  if (startX > endX + 50) {
+    // Deslizar para a esquerda
+    bootstrap.Carousel.getInstance(carousel).next();
+  } else if (startX < endX - 50) {
+    // Deslizar para a direita
+    bootstrap.Carousel.getInstance(carousel).prev();
+  }
+});
